6a6bc1273103bf2a8a1d33d8d3a4eabecd50adc2
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2022-03-17 17:45:09 -0300

    Aula 5 - Adicione seus filmes favoritos

---
+++ aula_05/script.js
@@ -0,0 +1,18 @@
+function adicionarFilme() {
+    var filmeFavorito = document.getElementById("filme").value;
+    if(filmeFavorito.endsWith(".jpg")) {
+        listarFilmesNaTela(filmeFavorito);
+    }else {
+        console.error('Endereco do filme inválido');
+    }
+
+    document.getElementById("filme").value = "";
+}
+
+function listarFilmesNaTela (filme) {
+    var elementoFilmeFavorito = "<img src=" + filme + ">";
+    var elementoListafilmes = document.getElementById("listaFilmes");
+    elementoListafilmes.innerHTML = elementoListafilmes.innerHTML + elementoFilmeFavorito;
+}
+
+
