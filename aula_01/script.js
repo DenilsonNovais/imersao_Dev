e250c83841013bd9931c9bd5990902999a9aac2e
Denilson Novais dos Santos <denilson_n.santos@hotmail.com>
2022-03-16 22:20:00 -0300

    Commit da Aula 1

---
+++ aula_01/script.js
@@ -0,0 +1,11 @@
+var nome = "Denilson";
+var notaPrimeiroBimestre = 9;
+var notaSegundoBimestre = 7;
+var notaTerceiroBimestre = 4;
+var notaQuartoBimestre = 2;
+
+var notaFinal = (notaPrimeiroBimestre + notaSegundoBimestre + notaTerceiroBimestre + notaQuartoBimestre)/4;
+
+// O metodo toFixed aredonda o resultado, passando as casas decimais
+var notaFixada = notaFinal.toFixed(1);
+
